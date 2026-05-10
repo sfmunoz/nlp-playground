40c3dc5f901275dd48f6842d1c806a6f6e5d9198
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-05-10 10:19:57 +0000

    run.sh: 'ANACONDA3_FOLDER' support created and 'conda' binary existence is verified

--- run.sh
+++ run.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
 set -e -o pipefail
+
+[ "${ANACONDA3_FOLDER}" = "" ] && ANACONDA3_FOLDER="/anaconda3"
+
+CONDA_BIN="${ANACONDA3_FOLDER}/bin/conda"
+
+if [ ! -x "${CONDA_BIN}" ]; then
+  echo "error: cannot execute '${CONDA_BIN}'"
+  exit 1
+fi
+
 cd "$(dirname "$0")"
-eval "$(/anaconda3/bin/conda shell.bash hook)"
+eval "$("${CONDA_BIN}" shell.bash hook)"
 set -x
 jupyter lab --notebook-dir=.
